6ed240969b42a002cf42d14c3c55f09b1ac7805d
Thabang <mokgolobothothabang@gmail.com>
2025-07-22 13:19:24 +0200

    Add to specify Python

--- whatsapp_bot/whatsapp_bot/settings.py
+++ whatsapp_bot/whatsapp_bot/settings.py
@@ -71,7 +71,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'whatsapp_bot.whatsapp_bot.wsgi.application'
+WSGI_APPLICATION = 'whatsapp_bot.wsgi.application'
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
